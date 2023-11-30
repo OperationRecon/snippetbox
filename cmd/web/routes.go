1464157f46799daa56285924cadfd9eb6f7d3ab3
OperationRecon <mohammadnaas203@gmail.com>
2023-11-30 19:54:27 +0200

    initial commit, after this the guided excersises will be commited

---
+++ cmd/web/routes.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+	"snippetbox.opre.net/ui"
+)
+
+func (app *application) routes() http.Handler {
+	// create server router
+	router := httprouter.New()
+
+	// create file server to handle serveing out of ui/static/
+	fileServer := http.FileServer(http.FS(ui.Files))
+
+	// handle serving the static files
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
+
+	// Custom handler for 404s
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFoundError(w)
+	})
+
+	// Create a new middleware chain containing the middleware specific to our
+	// dynamic application routes.
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
+
+	// route for other handlers
+	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodGet, "/ping", http.HandlerFunc(ping))
+
+	// Chain for user-protected routes
+	protected := dynamic.Append(app.requireAuthentication)
+
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
+
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(router)
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module snippetbox.opre.net
+
+go 1.21.4
+
+require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/justinas/alice v1.2.0
+
+require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/go-playground/form/v4 v4.2.1
+
+require (
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20231113091146-cef4b05350c8
+	github.com/alexedwards/scs/v2 v2.7.0
+)
+
+require golang.org/x/crypto v0.16.0
+
+require github.com/justinas/nosurf v1.1.1
